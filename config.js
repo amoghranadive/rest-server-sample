2d9e0a5d0736bd1388519fd4ee378e644f58c891
Amogh Ranadive <Amogh.Ranadive@redi.com>
2017-03-13 12:57:26 -0400

    added facebook OAuth

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '1266599636709560',
+        clientSecret: '8db46ee2c5f7cffe6f025424b5d56ac7',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -11,6 +11,37 @@ router.get('/', Verify.verifyOrdinaryUser, Verify.verifyAdmin, function(req, res
     });
 });
 
+router.get('/facebook', passport.authenticate('facebook'),
+  function(req, res){});
+
+router.get('/facebook/callback', function(req,res,next){
+    console.log('callback');
+    passport.authenticate('facebook', function(err, user, info) {
+        console.log('authenticate');
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            return res.status(401).json({
+                err: info
+            });
+        }
+        req.logIn(user, function(err) {
+            if (err) {
+                return res.status(500).json({
+                    err: 'Could not log in user'
+                });
+            }
+            var token = Verify.getToken(user);
+            res.status(200).json({
+                status: 'Login successful!',
+                success: true,
+                token: token
+            });
+        });
+    })(req,res,next);
+});
+
 router.post('/register', function(req, res) {
     User.register(new User({ username : req.body.username }),
                             req.body.password, function(err, user) {
